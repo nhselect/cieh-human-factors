edfbc3b60b2a98fd3a87a9363e7fa7a7f66fc233
timberrr <timberrr@users.noreply.github.com>
2022-08-10 10:27:48 +0100

    first commit

---
+++ components/LtlcHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="nhsuk-header nhsuk-header" role="banner">
+    <div class="nhsuk-width-container-fluid nhsuk-header__container">
+      <div class="nhsuk-header__logo nhsuk-header__logo--only">
+        <a class="nhsuk-header__link" href="https://ergonomics.org.uk/" aria-label="CIEHF website">
+          <img class="cieh-logo" src="../assets/img/ciehf-logo.png" />
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import 'node_modules/nhsuk-frontend/packages/components/header/header';
+
+.nhsuk-header {
+  background-color: #fff;
+}
+
+.nhsuk-header__container {
+  padding: 8px 0;
+}
+
+.nhsuk-header__link {
+  height: auto;
+  width: auto;
+}
+
+.cieh-logo {
+  width: 260px;
+}
+</style>
